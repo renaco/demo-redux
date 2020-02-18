c9ee6774a3883593d35013b6a8b671c06b4e2c18
renaco <renanromero@gmail.com>
2020-02-17 21:15:24 -0500

    feat: add ice cream reducer

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 function buyCake() {
   return {
@@ -10,10 +11,17 @@ function buyCake() {
   }
 }
 
+function buyIcreCream() {
+  return {
+    type: BUY_ICECREAM
+  }
+}
+
 // ( previousState, action ) => newState
 
 const initialState = {
-  numOfCakes: 10
+  numOfCakes: 10,
+  numOfIceCreams: 20
 }
 
 const reducer = (state = initialState, action) => {
@@ -21,6 +29,10 @@ const reducer = (state = initialState, action) => {
     case BUY_CAKE: return {
       ...state,
       numOfCakes: state.numOfCakes - 1
+    }
+    case BUY_ICECREAM: return {
+      ...state,
+      numOfIceCreams: state.numOfIceCreams - 1
     }
     default: return state
   }
@@ -34,5 +46,7 @@ const unsubscribe = store.subscribe(() => console.log('Update state', store.getS
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIcreCream());
+store.dispatch(buyIcreCream());
 
 unsubscribe()
